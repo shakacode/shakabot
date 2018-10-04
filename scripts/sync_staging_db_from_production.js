d19db6fc65932a08556d035fec322dcf46822c46
Mario Perez <mapreal19@gmail.com>
2018-10-04 13:22:41 +0200

    Command help

--- scripts/sync_staging_db_from_production.js
+++ scripts/sync_staging_db_from_production.js
@@ -1,11 +1,15 @@
+// Commands:
+//    shakabot hc:sync-staging-db-from-production
+
 const { spawn } = require('child_process');
 
 module.exports = robot => {
-  robot.respond(/sync staging db from production/i, response => {
-    response.send("Ok. Syncying staging DB...");
+  robot.respond(/hc:sync-staging-db-from-production/i, response => {
+    response.send('DB staging sync: start...');
     build = spawn('/bin/bash', ['./scripts/test.sh']);
 
-    build.stdout.on('data', (data) => robot.messageRoom('#bot-log', data.toString()));
-    build.stderr.on('data', (data) => robot.messageRoom('#bot-log', data.toString()));
+    build.stdout.on('data', data => robot.messageRoom('#bot-log', data.toString()));
+    build.stdout.on('close', () => response.send('DB staging sync: done'));
+    build.stderr.on('data', data => robot.messageRoom('#bot-log', data.toString()));
   });
 };
