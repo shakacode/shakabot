1e805c971d110e14cb6ae9b4756578f72a32143d
Sergey Tarasov <dzirtusss@gmail.com>
2020-04-22 14:23:13 +0300

    Upgrade to hichee

--- scripts/sync_staging_db_from_production.sh
+++ scripts/sync_staging_db_from_production.sh
@@ -1,5 +1,5 @@
-STAGING='hawaiichee-staging'
-PRODUCTION='hawaiichee-production'
+STAGING='hichee-staging'
+PRODUCTION='hichee-production'
 
 heroku maintenance:on --app $STAGING
 heroku pg:copy $PRODUCTION::DATABASE DATABASE --confirm $STAGING --app $STAGING
@@ -7,5 +7,5 @@ heroku run rails db:migrate --app $STAGING
 heroku run rails runner Rails.cache.clear --app $STAGING
 heroku run 'rails runner "Sidekiq.redis(&:flushdb)"' --app $STAGING
 heroku run rake stripe_sync:validate_stripe_data --app $STAGING
-aws s3 sync --delete s3://hc-activestorage-prod s3://hc-activestorage-staging 
+aws s3 sync --delete s3://hc-activestorage-prod s3://hc-activestorage-staging
 heroku maintenance:off --app $STAGING
