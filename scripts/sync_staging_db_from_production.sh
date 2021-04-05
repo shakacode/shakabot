380b293a7e37c7719d7ddb2042810cb6a5cee488
Sergey Tarasov <dzirtusss@gmail.com>
2021-04-05 11:41:17 +0300

    Fix DB readonly permissions

--- scripts/sync_staging_db_from_production.sh
+++ scripts/sync_staging_db_from_production.sh
@@ -3,6 +3,9 @@ PRODUCTION='hichee-production'
 
 heroku maintenance:on --app $STAGING
 heroku pg:copy $PRODUCTION::DATABASE DATABASE --confirm $STAGING --app $STAGING
+heroku pg:psql --app $STAGING --command 'GRANT USAGE ON SCHEMA PUBLIC TO readonly;'
+heroku pg:psql --app $STAGING --command 'GRANT SELECT ON ALL TABLES IN SCHEMA PUBLIC TO readonly;'
+heroku pg:psql --app $STAGING --command 'ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO readonly;'
 heroku run rails db:migrate --app $STAGING
 heroku run rails runner Rails.cache.clear --app $STAGING
 heroku run 'rails runner "Sidekiq.redis(&:flushdb)"' --app $STAGING
